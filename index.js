3dc46ca1d0e0eb74a74e4d9100bfb48bd2bcc4f3
Alfons2002 <alfonsdermawan2002@gmail.com>
2024-06-23 01:43:51 +0700

    push

--- index.js
+++ index.js
@@ -113,38 +113,55 @@ app.get("/dataPengantaran/:idKurir", (req, res) => {
       console.log(err);
     });
 });
-app.get("/riwayat/:idKurir", (req, res) => {
-  const idKurir = req.params.idKurir;
-  sequelize
-    .query(
-      `SELECT 
-        pr.id_data_riwayat ,
-        pr.id_kurir,
-        pr.waktu_pengiriman,
-        pr.tanggal_pengiriman,
-        pr.status_pengiriman,
-        pr.nomor_resi,
-        pr.Alamat_Tujuan,
-        pr.Nama_Pengiriman,
-        dk.nama AS nama_kurir,
-        dk.handphone AS handphone_kurir,
-        dk.email,
-        dk.password
-      FROM riwayat pr
-      JOIN data_kurir dk ON pr.id_kurir = dk.id_kurir
-      WHERE dk.id_kurir = :idKurir`,
-      {
-        replacements: { idKurir: idKurir },
-        type: sequelize.QueryTypes.SELECT,
-      }
-    )
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+app.post('/riwayat', (req, res) => {
+  const { id_kurir, nomor_resi, Alamat_Tujuan, Nama_Pengiriman, status_pengiriman } = req.body;
+
+  sequelize.query(`
+    INSERT INTO riwayat (id_kurir, nomor_resi, alamat_tujuan, nama_pengiriman, status_pengiriman, waktu_pengiriman, tanggal_pengiriman)
+    VALUES (:id_kurir, :nomor_resi, :Alamat_Tujuan, :Nama_Pengiriman, :status_pengiriman, NOW(), CURDATE())
+  `, {
+    replacements: {
+      id_kurir,
+      nomor_resi,
+      Alamat_Tujuan,
+      Nama_Pengiriman,
+      status_pengiriman,
+    },
+    type: sequelize.QueryTypes.INSERT
+  })
+  .then(() => {
+    res.status(201).send('Data sent to riwayat successfully');
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error sending data to riwayat');
+  });
 });
+
+app.delete('/pengantaran_paket/:id', async (req, res) => {
+  const idPengantaran = req.params.id;
+  
+  try {
+    const result = await sequelize.query(
+      'DELETE FROM pengantaran_paket WHERE Id_pengantaran_paket = :idPengantaran',
+      {
+        replacements: { idPengantaran },
+        type: sequelize.QueryTypes.DELETE
+      }
+    );
+    
+    if (result[1] > 0) {
+      res.status(200).send('Data pengantaran paket berhasil dihapus');
+    } else {
+      res.status(404).send('Data pengantaran paket tidak ditemukan');
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Gagal menghapus data pengantaran paket');
+  }
+});
+
+
 
 app.get("/dataRiwayat2", (req, res) => {
   sequelize
